b8387e6511fb26c0abe57d7e55b80f85079cb1d4
Eneo Juliano Bovino <eneo.bovino@gruposplice.com.br>
2018-05-15 08:37:52 -0300

    Criação dos arquivos em custom_libs

---
+++ custom_libs/__init__.py
@@ -0,0 +1,1 @@
+import eneo_functions
---
+++ custom_libs/eneo_functions.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+
+def get_version():
+    '''
+    Exibe a versão da biblioteca.
+    '''
+    print("eneo_functions 1.0")
